0f06f495b471a1b64e71e3a5d15f103201753401
z-s-kusz <z.s.kusz@gmail.com>
2025-09-03 23:14:26 -0500

    initial commit

---
+++ src/pages/trending.ts
@@ -0,0 +1,37 @@
+export const prerender = false;
+import type { APIRoute } from "astro";
+import { youtube } from '@googleapis/youtube';
+
+const getTrendingVideos = async (regionCode: string) => {
+    const youtubeClass = youtube({
+        version: 'v3',
+        auth: import.meta.env.GOOGLE_API_KEY,
+    });
+
+    try {
+        const response = await youtubeClass.videos.list({
+            part: ['snippet', 'statistics'],
+            chart: 'mostPopular',
+            regionCode,
+            maxResults: 4, // 50
+        });
+
+        return response.data.items;
+    } catch (error) {
+        console.error('Error fetching trending videos:', error);
+    }
+};
+
+export const GET:APIRoute = async ({ request }) => {
+    const url = new URL(request.url);
+    const region = url.searchParams.get('region') || 'US';
+
+    try {
+        const videos = await getTrendingVideos(region);
+
+        return Response.json(JSON.stringify({ videos }));
+    } catch (err) {
+        console.error('500 error in get', err);
+        return Response.error();
+    }
+}
